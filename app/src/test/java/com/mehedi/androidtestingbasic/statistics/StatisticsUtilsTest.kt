82bf0476ee78e6a1c36bed5720bc121144b06cff
mehedidevs <mehedi.hasan.app.dev@gmail.com>
2024-05-24 00:24:37 +0600

    hamcrest teast assert library added

--- app/src/test/java/com/mehedi/androidtestingbasic/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/mehedi/androidtestingbasic/statistics/StatisticsUtilsTest.kt
@@ -1,30 +1,29 @@
 package com.mehedi.androidtestingbasic.statistics
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class StatisticsUtilsTest {
-    
+    // if there is only task in the list with 0 completed & 100 active
+    // then return the 40, 60
     @Test
     fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
-        
-        // Create an active tasks (the false makes this active)
         val tasks = listOf<Task>(
             Task("title", "desc", isCompleted = false)
         )
-        // Call our function
         val result = getActiveAndCompletedStats(tasks)
         
-        // Check the result
         assertEquals(result.completedTasksPercent, 0f)
         assertEquals(result.activeTasksPercent, 100f)
     }
     
+    // if there are 5 task in the list with 2 completed & 3 active
+    // then return the 40, 60
     @Test
     fun getActiveAndCompletedStats_both_returns_forty_sixty() {
-        
-        // Create 5  tasks , 2 is completed , 3 is active
         val tasks = listOf<Task>(
             Task("title", "desc", isCompleted = true),
             Task("title", "desc", isCompleted = true),
@@ -32,12 +31,34 @@ class StatisticsUtilsTest {
             Task("title", "desc", isCompleted = false),
             Task("title", "desc", isCompleted = false),
         )
-        // Call our function
         val result = getActiveAndCompletedStats(tasks)
         
-        // Check the result
         assertEquals(result.completedTasksPercent, 40f)
         assertEquals(result.activeTasksPercent, 60f)
+    }
+    
+    // if there is no task list is empty
+    // then return the 0, 0
+    @Test
+    fun getActiveAndCompletedStats_tasks_empty_returns_zero_zero() {
+        val tasks = emptyList<Task>()
+        
+        val result = getActiveAndCompletedStats(tasks)
+        
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 0f)
+    }
+    
+    
+    // if there is no task list is null
+    // then return the 0, 0
+    @Test
+    fun getActiveAndCompletedStats_tasks_null_returns_zero_zero() {
+        val tasks = null
+        val result = getActiveAndCompletedStats(tasks)
+        
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(0f))
     }
     
 }
